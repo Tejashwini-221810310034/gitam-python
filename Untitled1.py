0d6b4f65e4a1fb06173efcfa6266403b2b507b80
Tejashwini-221810310034 <52528026+Tejashwini-221810310034@users.noreply.github.com>
2019-07-10 17:19:03 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Print("Hello Gitam")
+
+
+# In[2]:
+
+
+print("hello gitam")
+
+
+# In[3]:
+
+
+print("hyderabad")
+print("hello gitam")
+
+
+# In[4]:
+
+
+print("hello,hey","how r u?")
+
+
+# In[5]:
+
+
+print("hello,gitam","|||",end=" ")
+print("hyderabad",end="|||")
+print("python programming")
+
+
+# In[6]:
+
+
+n1=100
+print(n1)
+
+
+# In[7]:
+
+
+a=b=c=20
+a1,b1,c1=111,222,333
+print(n1)
+print(a,b,c)
+print(a1,b1,c1)
+
+
+# In[8]:
+
+
+a=100;
+s1="python"
+s2='p'
+f1=10,2
+print(a,s1,s2,f1)
+print(type(a),type(s1),type(s2),type(f1))
+
+
+# In[9]:
+
+
+i=100
+print(type(1))
+s1=str(i)
+print(type(s1))
+f1=float(i)
+print(type(f1))
+
+
+# In[10]:
+
+
+s1="100"
+print(type(s1))
+a=int(s1)
+print(type(a))
+f=1.5
+a1=int(f)
+print(type(a1))
+print(a1)
+
+
+# In[11]:
+
+
+a1=1234
+print(len(str(a1)))
+
+
+# In[12]:
+
+
+s1=input("enter your name")
+print(s1)
+print(type(s1))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+s1=int(input("enter a number"))
+print(s1)
+
+
+# In[ ]:
+
+
+s1=int(input("enter a number"))
+print(s1)
+
+
+# In[ ]:
+
+
+
+
---
+++ Untitled2.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+s1=int(input("enter a number"))
+print(s1)
+
+
+# ## haseena
+# # anusha
+# # teju
+# # praj
+# # thrisha
+
+# In[2]:
+
+
+print(7+6)
+print(14-1)
+print(68/2)
+print(3%4)
+
+
+# In[3]:
+
+
+priny(200/8)
+
+
+# In[4]:
+
+
+print(200//8)
+
+
+# In[5]:
+
+
+print(7+9*2/6%4)
+
+
+# In[6]:
+
+
+x=10
+a1=x>15
+print(a1)
+
+
+# In[7]:
+
+
+i=100
+a1=(i>15)and(i<800)
+a2=(i>15)and(i>800)
+print(a1)
+print(a2)
+
+
+# In[8]:
+
+
+n=int(input("enter s number"))
+if n%2==0;
+print("even")
+else
+print("odd")
+
+
+# In[9]:
+
+
+n=int(input("enter s number"))
+if n%2 == 0;
+print("even")
+else
+print("odd")
+
+
+# In[10]:
+
+
+n=int(input("enter s number"))
+if n%2 == 0;
+print("even")
+else
+print("odd")
+
+
+# In[11]:
+
+
+n=int(input("enter s number"))
+if n%2 == 0;
+print("even")
+else
+print("odd")
+
+
+# In[12]:
+
+
+n=int(input("enter a number"))
+if n%2 == 0;
+print("even")
+else
+print("odd")
+
+
+# In[13]:
+
+
+n=int(input("enter a number"))
+if n%2 == 0:
+    print("even")
+else:
+    print("odd")
+
+
+# In[14]:
+
+
+n=int(input("enter a number"))
+if n%3 == 0 and n%5 == 0:
+    print("yes")
+else:
+    print("no")
+    
+
+
+# In[15]:
+
+
+n=int(input("enter a number"))
+if n%3 == 0 and n%5 == 0:
+    print("yes")
+else:
+    print("no")
+
+
+# In[18]:
+
+
+n=int(input("enter a number"))
+if n == 0:
+    print("zero")
+elif n>0:
+    print("positive number")
+else:
+    print("negative number")
+    
+
+
+# In[19]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
